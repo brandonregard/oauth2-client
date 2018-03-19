f81c12bc302ca04401644928292fb96a74c2795f
Brandon Regard <brandon@printpoint.com>
2018-03-19 15:09:32 -0400

    Patches to make fork work with 4D v15.4HF3 and PHP 5.4.11.

--- src/Provider/AbstractProvider.php
+++ src/Provider/AbstractProvider.php
@@ -266,7 +266,9 @@ abstract class AbstractProvider
     {
         // Converting bytes to hex will always double length. Hence, we can reduce
         // the amount of bytes by half to produce the correct length.
-        return bin2hex(random_bytes($length / 2));
+        $bytes = openssl_random_pseudo_bytes($length/2, $strong); //BTR
+
+        return bin2hex($bytes);
     }
 
     /**
